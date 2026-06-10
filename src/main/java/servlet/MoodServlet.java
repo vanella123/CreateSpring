1191a145a0c61d2a8b28879e2f75d4478c055061
vanella123 <raelisonvanella@gmail.com>
2026-06-10 15:55:33 +0300

    creation_servlet

--- src/main/java/servlet/MoodServlet.java
+++ src/main/java/servlet/MoodServlet.java
@@ -3,12 +3,38 @@ import java.io.*;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
-//@WebServlet("/mood")
+
+@WebServlet("/")
 public class MoodServlet extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) 
+        throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        // 1. On récupère l'URL tapée par l'utilisateur
+        String requestURI = request.getRequestURI();
+        
+        // 2. On l'affiche dans la console du serveur (pour le développeur)
+        System.out.println("URL Interceptée : " + requestURI);
+        
+        // 3. On l'affiche sur la page web (pour tester)
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<html><body>");
+            out.println("<h1>Mon Framework Spring Maison 🚀</h1>");
+            out.println("<p>Tu as demandé l'URL : <strong>" + requestURI + "</strong></p>");
+            out.println("</body></html>");
+        }
+    } 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
     }
 
 }
+    
--- todo.md
+++ todo.md
@@ -1,8 +1,8 @@
-- architecture dossier 
-- creation de repository 
-- mettre le projet sur github 
+- architecture dossier [ok]
+- creation de repository [ok]
+- mettre le projet sur github [ok]
 ## But : terena mandalo amnio servlet io daoly ny url rehetra 
-- apprendre le pull request 
-- apprendre c'est quoi process request , print out (url)
+- apprendre le pull request : elle permet a ce que les autres dev puissent donner leur avis sur la modification faites par les autres users 
+- apprendre c'est quoi process request , print out (url) 
 #### Comment : - on met dans xml : mapping le lien du framework , ce que le deploy.sh doit sortir c'est un fichier .jar , urlParam
 - creer une application web pour tester que le framework marche 
